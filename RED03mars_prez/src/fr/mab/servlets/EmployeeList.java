5c75fd221f3eb698158afb4ba883b4aa0698c0f5
imie <imie@imie-dev-debian>
2015-03-03 12:48:25 +0100

    Projet RED - 3mars_midi

---
+++ RED03mars_prez/src/fr/mab/servlets/EmployeeList.java
@@ -0,0 +1,57 @@
+package fr.mab.servlets;
+
+
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.mab.model.Employee;
+import fr.mab.services.EmployeeServiceLocal;
+
+/**
+ * Servlet implementation class AllPersonne
+ */
+@WebServlet("/liste")
+public class EmployeeList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	EmployeeServiceLocal employeeService;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public EmployeeList() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		List<Employee> findAll = employeeService.findAll();
+		request.setAttribute("employees", findAll);
+		request.getRequestDispatcher("/WEB-INF/employeeList.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
