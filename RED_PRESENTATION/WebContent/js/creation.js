e5cbdc89f5c305054a14e8e482bcbd55e6ba2c1e
David Mabillot <david@mabillot.com>
2015-03-10 12:53:50 +0100

    ajout creation.js

---
+++ RED_PRESENTATION/WebContent/js/creation.js
@@ -0,0 +1,33 @@
+
+
+
+function creation()
+{
+
+
+
+
+  var dataformulaire = {
+           name : $("#inputLastName").val(),
+            lastname : $("#inputFirstName").val()
+        }
+
+alert(dataformulaire.name +" "+dataformulaire.lastname );
+alert(JSON.stringify(dataformulaire));
+
+$.ajax({
+            url: 'http://localhost:8080/RED_WEBSERVICE/rest/enduser',
+            type: 'POST',
+            dataType: 'json',
+             contentType: "application/json",
+              data: JSON.stringify(dataformulaire),
+            success: function (data) {
+              
+            }
+          
+        });
+
+
+}
+
+
