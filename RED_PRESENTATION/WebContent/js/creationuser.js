0df340159e943ac2189c3bb5de13bb9bf1d7c817
imie <imie@imie-dev-debian>
2015-03-11 16:59:44 +0100

    push pour kadia

---
+++ RED_PRESENTATION/WebContent/js/creationuser.js
@@ -0,0 +1,36 @@
+
+
+
+function creationuser()
+{
+
+
+
+
+  var dataformulaire = {
+           	name : $("#inputLastName").val(),
+            lastname : $("#inputFirstName").val(),
+           
+            
+
+        }
+
+alert(dataformulaire.name +" "+dataformulaire.lastname );
+alert(JSON.stringify(dataformulaire));
+
+$.ajax({
+            url: 'http://localhost:8080/RED_WEBSERVICE/rest/employee',
+            type: 'POST',
+            dataType: 'json',
+             contentType: "application/json",
+              data: JSON.stringify(dataformulaire),
+            success: function (data) {
+              
+            }
+          
+        });
+
+
+}
+
+
